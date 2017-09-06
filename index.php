27fcc3cf9678f8b8eaec07783bb416dbb844b321
norajs <norajsinger@gmail.com>
2017-09-06 00:04:52 -0400

    Charlie website home page design

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+    <link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
+
+
+  </head>
+  <body>
+      
+      
+
+      
+      
+      <div class="container-fluid">
+          <div class="row" id="first_row">
+              
+              <div id="one" class="col-md-4 col-lg-5 ">
+                  <div id="logo">Charlie Serabian</div>
+                  
+              </div>
+              
+              <div id="two" class="col-md-4 col-lg-2  mt-lg-auto mb-lg-auto mt-md-auto mb-md-auto mt-sm-auto mb-sm-auto">
+                  <div id="text">
+                      <p><a  href="writing.php">Writing</a></p>
+                      <p><a href="portfolio.php">Portfolio</a></p>
+                      <p><a href="about.php">About</a></p> 
+                  </div>
+              </div>
+              
+              <div id="three" class="col-md-4 col-lg-5 "></div>
+              
+          </div>
+          
+          
+          <div class="row" id="second_row">
+              
+              <div id="one2" class=" col-lg-5 ">
+                  <div id="footer">
+                      &copy;Charlie Serabian 2017
+                      <i href="https://www.youtube.com/watch?v=vBYSO76EPSM" id="fa1" class="fa fa-facebook-square" aria-hidden="true"></i>
+                      <i id="fa2" class="fa fa-instagram" aria-hidden="true"></i>
+                  </div>
+                  
+              </div>
+              
+              <div id="two2" class="col-lg-2">
+                  <div id="social">
+                      
+                  </div>
+              </div>
+              
+              <div id="three2" class="col-lg-5">
+                  
+              </div>
+              
+          </div>
+          
+          
+         
+      </div>
+      
+    
+      
+      
+    
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+      
+      <script>
+          document.getElementById("fa1").onclick= function() {
+              location.href = "https://www.youtube.com/watch?v=gXmrlB9n9sE";
+          }
+          
+          document.getElementById("fa2").onclick= function() {
+              location.href = "https://www.youtube.com/watch?v=PG-xzJZctKU";
+          }
+      
+      </script>
+  </body>
+</html>
